bf6b8244f98652494dafe9e293a504b562bee285
Rayff de Souza <rayfficaro@gmail.com>
2021-02-21 09:18:23 -0300

    Adição do exercício 2 da seção 6

---
+++ SECAO-6-EXERCICIO-2/index.py
@@ -0,0 +1,18 @@
+"""
+GEEK UNIVERSITY - Exercício
+	- Faça um programa que escreva de 1 até 100 duas vezes, sendo a primeira vez com o loop FOR e a
+	segunda com o loop WHILE.
+"""
+
+print('FOR'.center(20, '_'))
+for n in range(1, 101):
+	print(n)
+print('FOR'.center(20, '_'))
+
+print('\n')
+
+contador = 1
+print('WHILE'.center(20, '_'))
+while contador < 101:
+	print(contador)
+print('WHILE'.center(20, '_'))
